06ea7b79397524641dfa1ff3ff68cd11401a9191
Matt Corallo <git@bluematt.me>
2017-10-27 18:22:56 -0400

    Add useful-for-dos "reason" field to CValidationState
    This is a first step towards cleaning up our DoS interface - make
    validation return *why* something is invalid, and let net_processing
    figure out what that implies in terms of banning/disconnection/etc.

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -22,6 +22,32 @@ static const unsigned char REJECT_NONSTANDARD = 0x40;
 static const unsigned char REJECT_INSUFFICIENTFEE = 0x42;
 static const unsigned char REJECT_CHECKPOINT = 0x43;
 
+/** A "reason" why something was invalid, suitable for (possibly) getting angry
+  * (and retaliating) at the provider of the object in question (ie banning
+  * peers).
+  * These are much more granular than the rejection codes, which may be more
+  * useful for some other use-cases.
+  */
+enum class ValidationInvalidReason {
+    // txn and blocks:
+    VALID,           //!< not actually invalid
+    CONSENSUS,       //!< invalid by consensus rules (excluding any below reasons)
+    SOFT_FORK,       //!< invalid by non-ancient soft-fork rules (in some rare cases possibly by original consensus rules, too)
+    UNKNOWN_INVALID, //!< this object was cached as being invalid, but we don't know why
+    // Only blocks:
+    MUTATED,         //!< the block's data didn't match the data committed to by the PoW
+    MISSING_PREV,    //!< We don't have the previous block the checked one is built on
+    INVALID_PREV,    //!< A block this one builds on is invalid
+    BAD_TIME,        //!< block timestamp was > 2 hours in the future (or our clock is bad)
+    CHECKPOINT,      //!< the block failed to meet one of our checkpoints
+    // Only loose txn:
+    NOT_STANDARD,    //!< didn't meet our local policy rules
+    MISSING_INPUTS,  //!< a transaction was missing some of its inputs (or its inputs were spent at < coinbase maturity height)
+    WITNESS_MUTATED, //!< tx might be missing a witness or witness may have been malleated
+    CONFLICT,        //!< tx already in mempool or conflicts with an existing one (which is in chain or in mempool and RBF failed)
+    MEMPOOL_LIMIT,   //!< violated mempool's fee/size/descendant/etc limits
+};
+
 /** Capture information about block/transaction validation */
 class CValidationState {
 private:
@@ -30,17 +56,19 @@ private:
         MODE_INVALID, //!< network rule violation (DoS value may be set)
         MODE_ERROR,   //!< run-time error
     } mode;
+    ValidationInvalidReason reason;
     int nDoS;
     std::string strRejectReason;
     unsigned int chRejectCode;
     bool corruptionPossible;
     std::string strDebugMessage;
 public:
-    CValidationState() : mode(MODE_VALID), nDoS(0), chRejectCode(0), corruptionPossible(false) {}
-    bool DoS(int level, bool ret = false,
+    CValidationState() : mode(MODE_VALID), reason(ValidationInvalidReason::VALID), nDoS(0), chRejectCode(0), corruptionPossible(false) {}
+    bool DoS(int level, ValidationInvalidReason reasonIn, bool ret = false,
              unsigned int chRejectCodeIn=0, const std::string &strRejectReasonIn="",
              bool corruptionIn=false,
              const std::string &strDebugMessageIn="") {
+        reason = reasonIn;
         chRejectCode = chRejectCodeIn;
         strRejectReason = strRejectReasonIn;
         corruptionPossible = corruptionIn;
@@ -51,10 +79,10 @@ public:
         mode = MODE_INVALID;
         return ret;
     }
-    bool Invalid(bool ret = false,
+    bool Invalid(ValidationInvalidReason _reason, bool ret = false,
                  unsigned int _chRejectCode=0, const std::string &_strRejectReason="",
                  const std::string &_strDebugMessage="") {
-        return DoS(0, ret, _chRejectCode, _strRejectReason, false, _strDebugMessage);
+        return DoS(0, _reason, ret, _chRejectCode, _strRejectReason, false, _strDebugMessage);
     }
     bool Error(const std::string& strRejectReasonIn) {
         if (mode == MODE_VALID)
@@ -84,6 +112,8 @@ public:
     void SetCorruptionPossible() {
         corruptionPossible = true;
     }
+    ValidationInvalidReason GetReason() const { return reason; }
+    int GetDoS() const { return nDoS; }
     unsigned int GetRejectCode() const { return chRejectCode; }
     std::string GetRejectReason() const { return strRejectReason; }
     std::string GetDebugMessage() const { return strDebugMessage; }
